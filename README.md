89501906af7e1e3cf9de11c7b8e0602535c98c60
lolinatorishere <78425011+Lolinatorishere@users.noreply.github.com>
2024-12-08 19:31:25 +0000

    Initial commit
     Changes to be committed:
    	new file:   .gitignore
    	new file:   README.md
    	new file:   day1/part1/input.txt
    	new file:   day1/part1/main.c
    	new file:   day1/part1/test.txt
    	new file:   day1/part2/input.txt
    	new file:   day1/part2/main.c
    	new file:   day1/part2/test.txt

---
+++ README.md
@@ -0,0 +1,5 @@
+# AOC 2024
+
+this is my AOC 2024 repository.
+
+wow generic ik 🧌
---
+++ day1/part1/main.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define isasciinum(STRING)\
+    if(STRING >= 48 && STRING <= 57)
+
+int clear_buffer(char *buffer){
+    for(int i = 0 ; i < 128 ; i++){
+        buffer[i] = '\0';
+    }
+    return 0;
+}
+
+int compare_ints(const void *a, const void *b) {
+    int64_t int_a = *(int64_t *)a;
+    int64_t int_b = *(int64_t *)b;
+
+    if (int_a < int_b) return -1;
+    else if (int_a > int_b) return 1;
+    else return 0;
+}
+
+void parce_numbers(char *input, int j, int *num1, int *num2){
+    char buffer[128];
+    int k = 0;
+    int which = 0;
+    for (int i = 0 ; i < j ; i++){
+        isasciinum(input[i]){
+            buffer[k] = input[i];
+            k++;
+        }else{
+            if(which == 0 && buffer[0] != '\n'){
+                *num1 = atoi(buffer);
+            }else{
+                *num2 = atoi(buffer);
+            }
+            which++;
+            clear_buffer(buffer);
+            k=0;
+        }
+    }
+}
+
+int main(){
+    char filedir[] = "input.txt";
+    FILE *file;
+    int filecheck = 1;
+    int arrMax = 0;
+    int64_t filesize = 0;
+    char fchar;
+
+    file = fopen(filedir, "r");
+
+    if(file == NULL){
+        printf("File Doesnt Exist");
+        return 1;
+    }
+    // get the ammount of numbers to allocate for memory
+    // arrMax
+    while(filecheck != 0){
+        fseek(file,filesize,SEEK_SET);
+        fchar = fgetc(file);
+        if(fchar == EOF){
+            filecheck = 0;
+        }
+        if(fchar == '\n'){
+            arrMax++;
+        }
+        filesize++;
+    }
+
+    int64_t *array1 = malloc(sizeof(int64_t)*arrMax);
+    int64_t *array2 = malloc(sizeof(int64_t)*arrMax);
+    filecheck = 1;
+    int i = 0, j = 0, arrPos = 0;
+    char buffer[128];
+    int num1 = 0;
+    int num2 = 0;
+
+    for(i = 0 ; i < filesize ; i++) {
+        fseek(file,i,SEEK_SET);
+        fchar = fgetc(file);
+        buffer[j] = fchar;
+        j++;
+        if(fchar == '\n'){
+            parce_numbers(buffer, j, &num1, &num2);
+            j = 0;
+            array1[arrPos] = num1;
+            array2[arrPos] = num2;
+            arrPos++;
+        }
+        if(i+1 == filesize){
+            parce_numbers(buffer, j, &num1, &num2);
+            j = 0;
+            array1[arrPos] = num1;
+            array2[arrPos] = num2;
+            arrPos++;
+        }
+    }
+    fclose(file);
+
+    qsort(array1, arrMax, sizeof(int64_t), compare_ints);
+    qsort(array2, arrMax, sizeof(int64_t), compare_ints);
+    int64_t total = 0, temp = 0;
+    
+    for(i = 0 ; i < arrMax; i++){
+        temp = array1[i] - array2[i];
+        if(temp < 0){
+            temp = temp * -1;
+        }
+        total+=temp;
+        temp = 0;
+    }
+    printf("%li\n",total);
+
+    free (array1);
+    free (array2);
+    return 0;
+}
