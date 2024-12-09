cdfe551030f65acd5636de40bb96bb385640d5e3
Gabriel Banks <78425011+Lolinatorishere@users.noreply.github.com>
2024-12-09 18:10:50 +0000

    Finished Day 2 part 2 answer was 514
     On branch master
     Your branch is ahead of 'origin/master' by 2 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   day1/part2/output.txt
    	modified:   day2/part1/main.c
    	new file:   day2/part1/output.txt
    	new file:   day2/part2/input.txt
    	new file:   day2/part2/main.c
    	new file:   day2/part2/main.out
    	new file:   day2/part2/output.txt
    	new file:   day2/part2/test.txt

--- day2/part2/main.c
+++ day2/part2/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define filedir "test.txt"
+#define filedir "input.txt"
 
 #define isasciinum(STRING)\
     if(STRING >= 48 && STRING <= 57)
@@ -13,16 +13,51 @@ int clear_buffer(char *buffer){
     return 0;
 }
 
+int is_safe(int64_t *input);
+
+void buffer_set(int64_t *input, int64_t *output, int index_skip, int maxindex){
+    int i = 0,j = 0;
+    for(i = 0 ; i <= maxindex ; i++){
+        if(i == index_skip){
+            i++;
+        }
+        output[j] = input[i];
+        j++;
+    }
+}
+
+int make_safe(int64_t *input, int line){
+    int arraysize = 0;
+    int issue_val = 0;
+    int value = 0;
+    while(input[arraysize] != -1){
+        arraysize++;
+    }
+    if(is_safe(input) == 1){
+        return 1;
+    }
+    int64_t *buffer = malloc(sizeof(int64_t)*arraysize);
+    for(int i = 0 ; i < arraysize ; i++){
+        buffer_set(input, buffer, i, arraysize);
+        if(is_safe(buffer) == 1){
+            printf("%i\n",line);
+            value = 1;
+            i = arraysize;
+            continue;
+        }
+    }
+    free(buffer);
+    return value;
+}
+
 int is_safe(int64_t *input){
     int64_t temp = 0;
     int i = 0;
     int directionFlag = 0;
     while(input[i+1] != -1){
         temp = input[i+1] - input[i];
-        printf("%li\n",temp);
         if(temp > 0 && temp <= 3){
             if(directionFlag != 0 && directionFlag != 1){
-                printf("\n");
                 return 0;
             }
             i++;
@@ -32,7 +67,6 @@ int is_safe(int64_t *input){
         }
         if(temp < 0 && temp >= -3){
             if(directionFlag != 0 && directionFlag != -1){
-                printf("\n");
                 return 0;
             }
             directionFlag = -1;
@@ -40,13 +74,10 @@ int is_safe(int64_t *input){
             temp = 0;
             continue;
         }
-        printf("\n");
         return 0;
     }
-    printf("\n");
     return 1;
 }
-
 void parce_numbers(char *input, int j, int64_t **array, int arrPos){
     char buffer[128];
     int numbers = 0;
@@ -126,7 +157,7 @@ int main(){
     fclose(file);
     int safe = 0;
     for(int i = 0 ; i < arrMax ; i++){
-        safe += is_safe(array1[i]);
+        safe += make_safe(array1[i], i);
     }
     free (array1);
     printf("safe: %i",safe);
